6a1cae99a7599e30477528ecbed90cb051977ff7
hutaochan99 <ahmadzhofir1808@gmail.com>
2026-06-09 13:59:18 +0700

    video stream "

--- app/components/YouTubeEmbed.js
+++ app/components/YouTubeEmbed.js
@@ -3,14 +3,14 @@
 import { useEffect } from 'react'
 
 export default function YouTubeEmbed({ url }) {
-  const streamUrl = process.env.NEXT_PUBLIC_STREAM_URL
+  const streamUrl = 'http://swiftness-undecided-empower.ngrok-free.dev/stream.html?src=tapo_kamera&mode=webrtc'
 
   useEffect(() => {
     console.log('=== DEBUG STREAM ===')
     console.log('streamUrl:', streamUrl)
     console.log(
       'iframe src:',
-      `${streamUrl}/stream.html?src=tapo_kamera&mode=webrtc`
+      `${streamUrl}/ `
     )
 
     setTimeout(() => {
@@ -32,12 +32,12 @@ export default function YouTubeEmbed({ url }) {
 
   return (
     <div
-      className="relative pb-[56.25%] h-0 border-4 border-red-500"
+      className="relative pb-[56.25%] h-0 border"
       style={{ minHeight: '450px' }}
     >
       <iframe
         className="absolute top-0 left-0 w-full h-full rounded-md"
-        src={`${streamUrl}/stream.html?src=tapo_kamera&mode=webrtc`}
+        src={`${streamUrl}/ `}
         title="Camera Stream"
         onLoad={() => {
           console.log('iframe loaded')
